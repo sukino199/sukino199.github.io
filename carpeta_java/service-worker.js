02f36d1b06a5d930d9d1514523839b2653a54626
sukino199 <aguilarisaias522@gmail.com>
2025-03-20 18:27:57 -0600

    Update service-worker.js

--- carpeta_java/service-worker.js
+++ carpeta_java/service-worker.js
@@ -17,7 +17,8 @@ self.addEventListener('install', event => {
 });
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response=>{
+        caches.match(event.request)
+        .then(response=>{
             return response || fetch(event.request);
         })
     );
