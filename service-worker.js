2ae0c2faa961ab8946af702b775cfe5462d81232
sukino199 <aguilarisaias522@gmail.com>
2025-03-25 17:02:51 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tareas-cache-v1';
+const CACHE_NAME = 'tareas-cache-v2'; // Cambia el nombre del caché para invalidar el antiguo
 const urlsToCache =[
     './',
     './index.html',
@@ -15,11 +15,30 @@ self.addEventListener('install', event => {
         })
     );
 });
+
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
         .then(response=>{
             return response || fetch(event.request);
+        }).catch(error => {
+            console.error('Fetch failed; returning offline page instead.', error);
+            // Puedes devolver una página de error personalizada aquí
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
